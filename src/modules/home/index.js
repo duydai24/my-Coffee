2f47af85f7dabc5cec916e7ac030aaed2f65eb9c
duydai24 <nguyenduydai24@gmail.com>
2022-05-31 15:08:30 +0700

    testDeploy3

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -1,12 +1,11 @@
 import React from "react";
+import Banner from "./banner/banner";
 import Backgroud from "./backgroud/backgroud";
-import Banner from "./Banner/banner";
 import Event from "./event/event";
 import Featured from "./featured/featured";
 import Introduce from "./introduce/introduce";
 import Products from "./products/products";
 import Stories from "./stories/stories";
-import Fade from "react-reveal/Fade";
 
 function Home() {
   return (
